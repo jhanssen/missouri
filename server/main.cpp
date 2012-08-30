89cc8c31fe96bffb27ddf0b11dd3ee91bcd3985d
Jan Erik Hanssen <jhanssen@gmail.com>
2012-08-30 00:47:59 -0700

    Make use of the TcpServer

--- server/main.cpp
+++ server/main.cpp
@@ -1,9 +1,17 @@
 #include "GDICapturer.h"
 #include "Encoder.h"
+#include "TcpServer.h"
+#include "TcpSocket.h"
 #include "UdpSocket.h"
 #include <Windows.h>
 #include <Winsock2.h>
 #include <stdio.h>
+
+static bool newSocket(TcpSocket* socket, void* userData)
+{
+    Encoder* enc = static_cast<Encoder*>(userData);
+    printf("got connection, encoder %p\n", enc);
+}
 
 int main(int argc, char** argv)
 {
@@ -14,20 +22,23 @@ int main(int argc, char** argv)
         return 1;
     }
 
-/*
-    UdpSocket server;
-    if (!server.listen(21047)) {
-        WSACleanup();
-        return 1;
-    }
-*/
-
     GDICapturer cap;
     const uint8_t* buffer = cap.GetBuffer();
     const int32_t width = cap.GetWidth();
     const int32_t height = cap.GetHeight();
     const int32_t size = cap.GetImageSize();
     Encoder enc(buffer, width, height, size);
+
+    const uint16_t port = 21047;
+    TcpServer server;
+    server.setCallback(newSocket, &enc);
+    if (!server.listen(port)) {
+        fprintf(stderr, "Server listen failed\n");
+        WSACleanup();
+        return 1;
+    }
+    fprintf(stdout, "Server listening on port %u\n", port);
+
     const int sleepFor = 1000 / 60;
     for (;;) {
         cap.Capture();
