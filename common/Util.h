1231ddd5c82c5a1fdc0b271d9d8c458713e1176f
Jan Erik Hanssen <jhanssen@gmail.com>
2012-09-25 22:27:45 -0700

    Add a WaitCondition class (requires Vista on Windows systems)

--- common/Util.h
+++ common/Util.h
@@ -2,9 +2,9 @@
 #define UTIL_H
 
 #ifdef OS_Windows
-#include <Windows.h>
+# include <Windows.h>
 #else
-#include <pthread.h>
+# include <pthread.h>
 #endif
 
 class Thread
@@ -43,6 +43,25 @@ private:
     CRITICAL_SECTION mutex;
 #else
     pthread_mutex_t mutex;
+#endif
+    friend class WaitCondition;
+};
+
+class WaitCondition
+{
+public:
+    WaitCondition();
+    ~WaitCondition();
+
+    void wait(Mutex* mutex, int timeout = -1);
+    void signal();
+    void broadcast();
+
+private:
+#ifdef OS_Windows
+    CONDITION_VARIABLE cond;
+#else
+    pthread_cond_t cond;
 #endif
 };
 
@@ -93,6 +112,57 @@ inline void Mutex::unlock()
     LeaveCriticalSection(&mutex);
 #else
     pthread_mutex_unlock(&mutex);
+#endif
+}
+
+inline WaitCondition::WaitCondition()
+{
+#ifdef OS_Windows
+    InitializeConditionVariable(&cond);
+#else
+    pthread_cond_init(&cond, 0);
+#endif
+}
+
+inline WaitCondition::~WaitCondition()
+{
+    // apparently there's no need to delete CONDITION_VARIABLEs on Windows
+#ifndef OS_Windows
+    pthread_cond_destroy(&cond);
+#endif
+}
+
+inline void WaitCondition::wait(Mutex* mutex, int timeout)
+{
+#ifdef OS_Windows
+    SleepConditionVariableCS(&cond, &mutex->mutex, timeout == -1 ? INFINITE : timeout);
+#else
+    if (timeout == -1)
+        pthread_cond_wait(&cond, &mutex->mutex);
+    else {
+        timespec spec;
+        spec.tv_sec = timeout / 1000;
+        spec.tv_nsec = (timeout % 1000) * 1000000;
+        pthread_cond_timedwait(&cond, &mutex->mutex, &spec);
+    }
+#endif
+}
+
+inline void WaitCondition::signal()
+{
+#ifdef OS_Windows
+    WakeConditionVariable(&cond);
+#else
+    pthread_cond_signal(&cond);
+#endif
+}
+
+inline void WaitCondition::broadcast()
+{
+#ifdef OS_Windows
+    WakeAllConditionVariable(&cond);
+#else
+    pthread_cond_broadcast(&cond);
 #endif
 }
 
